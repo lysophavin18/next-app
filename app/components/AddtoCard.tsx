b9b84deabc891c64c2e208fb5543bd98e401b39c
sophavinly18 <https://github.com/lysophavin18/ProjectPratisCumY3>
2024-07-10 11:13:37 +0700

    NextJs

---
+++ app/components/AddtoCard.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddtoCard = () => {
+    return (
+        <>
+            <button className='btn btn-primary ' onClick={() => console.log('Click')}>Add to Card</button>
+        </>
+    )
+}
+
+export default AddtoCard
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import AddtoCard from './AddtoCard'
+
+
+
+const ProductCard = () => {
+    return (
+        < >
+            <AddtoCard />
+        </>
+    )
+}
+
+export default ProductCard
